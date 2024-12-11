e45056a1700c04cc117da8411899da96f5634341
Di-Mo-De-OH <dhelahep0115@gmail.com>
2024-12-12 00:46:21 +0900

    2024-12-12

---
+++ home.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="FullstackGPT Home",
+    page_icon="😶‍🌫️"
+)
+
+st.title("FullstackGPT Home")
+
+
+st.markdown(
+    """
+
+    #hello!
+
+    welcome to my FullstackGPT portfolio!
+
+    here are the apps i made:
+    - [ ] [DocumentGPT](/DocumentGPT)
+    - [ ] [PrivateGPT](/PrivateGPT)
+    - [ ] [QuizGPT](/QuizGPT)
+    - [ ] [SiteGPT](/SiteGPT)
+    - [ ] [MeetingGPT](/MeetingGPT)
+    - [ ] [InvestorGPT](/InvestorGPT)
+"""
+)
